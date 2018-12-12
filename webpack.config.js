eaad7c32cf5bba23d3b0c83007e7e6ba6780a0bd
Austen Wade <awade@merkleinc.com>
2018-12-12 00:03:05 -0700

    Fixed hero image not appearing

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,7 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/dist'
+        filename: 'bundle.js'
     },
     module: {
         rules: [
